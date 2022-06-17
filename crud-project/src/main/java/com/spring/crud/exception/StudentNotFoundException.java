80d7e7f0a57a428953c72dd5552065d5f561d65f
sunil kumar samal <sunilsamal322@gmail.com>
2022-06-17 14:49:28 +0530

    [Add]functionality for spring security basic authentication

--- crud-project/src/main/java/com/spring/crud/exception/StudentNotFoundException.java
+++ crud-project/src/main/java/com/spring/crud/exception/StudentNotFoundException.java
@@ -1,13 +1,14 @@
 package com.spring.crud.exception;
 
 
-public class StudentNotFoundException extends RuntimeException{
+public class StudentNotFoundException extends RuntimeException {
 
     private Integer studentId;
+    private String message;
 
-    public StudentNotFoundException(Integer studentId)
-    {
-        super(String.format("Student not found with id %s",studentId));
-        this.studentId=studentId;
+    public StudentNotFoundException(Integer studentId) {
+        super(String.format("Student not found with id %s", studentId));
+        this.studentId = studentId;
     }
 }
+
